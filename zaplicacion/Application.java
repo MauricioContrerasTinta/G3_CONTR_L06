31c8502ae13afd593e7ebf23224a3e0740238b44
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:48:31 -0500

    rehacer

--- zaplicacion/Application.java
+++ zaplicacion/Application.java
@@ -3,31 +3,6 @@ import ejercicio1.StackLink;
 import actividad1.ExceptionIsEmpty;
 
 public class Application {
-    public static boolean symbolBalancing(String s) {
-        StackLink<Character> stack = new StackLink<>();
-        for (int i = 0; i < s.length(); i++) {
-            char simbolo = s.charAt(i);
-            if (simbolo == '(' || simbolo == '[' || simbolo == '{') {
-                stack.push(simbolo);
-            }
-            else if (simbolo == ')' || simbolo == ']' || simbolo == '}') {
-                if (stack.isEmpty()) return false;
 
-                try {
-                    char tope = stack.pop();
-                        if (!coincide(tope, simbolo)) return false;
-                    } catch (ExceptionIsEmpty e) {
-                        return false;
-                    }
-                }
-            }
-            return stack.isEmpty();
-        }
-
-        private static boolean coincide(char apertura, char cierre) {
-            return  (apertura == '(' && cierre == ')') ||
-                    (apertura == '[' && cierre == ']') ||
-                    (apertura == '{' && cierre == '}');
-        }
 }
 
