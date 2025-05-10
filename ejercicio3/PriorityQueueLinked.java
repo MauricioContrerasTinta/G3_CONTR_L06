5e3693f3f1927c3b03f5fb5790f71febc7a9aea5
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:20:53 -0500

    contructor

--- ejercicio3/PriorityQueueLinked.java
+++ ejercicio3/PriorityQueueLinked.java
@@ -6,4 +6,12 @@ import actividad1.ExceptionIsEmpty;
 public class PriorityQueueLinked<E> implements PriorityQueue<E> {
     private Queue<E>[] colas; //arreglo de colas
     private int niveles; //cantidad de prioridades
+
+    public PriorityQueueLinked(int niveles) {
+        this.niveles = niveles;
+        this.colas = new Queue[niveles];
+        for (int i = 0; i < niveles; i++) {
+            this.colas[i] = new QueueLink<E>(); //se inicializa cada cola como una lista enlazada
+        }
+    }
 }
