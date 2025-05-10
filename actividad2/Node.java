34f699e2ad4e719643a46a142f5a22bc8fdf74af
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 12:06:31 -0500

    cosntrcutor

--- actividad2/Node.java
+++ actividad2/Node.java
@@ -2,5 +2,10 @@ package actividad2;
 
 public class Node<E> {
     private E data;         
-    private Node<E> next; 
+    private Node<E> next;
+    
+    public Node(E data) {
+        this.data = data;
+        this.next = null;
+    }
 }
