4336a0658d9ed3d5a8d85643de060e923849a844
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:38:16 -0500

    Se recorre cada carácter de la cadena

--- zaplicacion/Application.java
+++ zaplicacion/Application.java
@@ -4,4 +4,8 @@ import actividad1.ExceptionIsEmpty;
 
 public class Application {
     StackLink<Character> stack = new StackLink<>();
+
+    for (int i = 0; i < s.length(); i++) {
+        char simbolo = s.charAt(i);
+    }
 }
