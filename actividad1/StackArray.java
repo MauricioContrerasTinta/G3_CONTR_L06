189b04989035f78fe2cda23c944e9babfe6ba726
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 11:38:52 -0500

    cosntructor

--- actividad1/StackArray.java
+++ actividad1/StackArray.java
@@ -2,5 +2,10 @@ package actividad1;
 
 public class StackArray<E> implements Stack<E> {
     private E[] array;
-    private int tope; 
+    private int tope;
+    
+    public StackArray(int n){
+        this.array = (E[]) new Object[n];
+        this.tope = -1;
+    }
 }
