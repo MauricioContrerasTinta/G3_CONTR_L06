28313886d2b864982661f94c6c62b14b7b3ca387
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:13:53 -0500

    try

--- Test.java
+++ Test.java
@@ -10,5 +10,15 @@ public class Test {
         colaEnteros.enqueue(30);
 
         System.out.println("Cola actual: " + colaEnteros);
+
+        try {
+            System.out.println("Elemento eliminado: " + colaEnteros.dequeue());
+            System.out.println("Elemento al frente: " + colaEnteros.front());
+            System.out.println("Elemento al final: " + colaEnteros.back());
+        } catch (ExceptionIsEmpty e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Cola final: " + colaEnteros);
     }
 }
