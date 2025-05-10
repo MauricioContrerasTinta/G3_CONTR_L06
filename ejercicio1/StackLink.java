0a6e1b3357af656fa9399153dbfd3183faf8369d
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 14:58:09 -0500

    nodo simple para lista enlazada

--- ejercicio1/StackLink.java
+++ ejercicio1/StackLink.java
@@ -1,4 +1,15 @@
 package ejercicio1;
+import actividad1.Stack; //usamos la misma interfaz Stack de la actividad 1
+import actividad1.ExceptionIsEmpty;
+
+class Node<E> {
+    E dato;               
+    Node<E> siguiente;    
+    public Node(E dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+}
 
 public class StackLink {
     
