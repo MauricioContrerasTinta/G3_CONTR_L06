1035cd9fb6901727f5bf8a6a12f83d8d3c799097
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:03:23 -0500

    prueba con enteros

--- Test.java
+++ Test.java
@@ -3,5 +3,12 @@ import actividad1.Stack;
 import actividad1.ExceptionIsEmpty;
 
 public class Test {
-    
+    public static void main(String[] args) {
+                Stack<Integer> pilaEnteros = new StackLink<>();
+                pilaEnteros.push(10); 
+                pilaEnteros.push(20); 
+                pilaEnteros.push(30); 
+        
+                System.out.println("Pila de enteros: " + pilaEnteros);
+    }
 }
