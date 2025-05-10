66f5435c6ee14c0f3051d632b200d98002df7879
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:50:45 -0500

    Agrega pruebas para balanceo de corchetes usando StackLink

--- Test.java
+++ Test.java
@@ -1,5 +1,12 @@
 import zaplicacion.Application;
 
 public class Test {
-    
+    public static void main(String[] args) {
+        System.out.println(Application.symbolBalancing("()()()[()]()")); //true
+        System.out.println(Application.symbolBalancing("((()))[]"));     //true
+        System.out.println(Application.symbolBalancing("([])[]("));      //false
+        System.out.println(Application.symbolBalancing("([{)]}"));       //false
+        System.out.println(Application.symbolBalancing("["));            //false
+        System.out.println(Application.symbolBalancing("[][][]{{{}}}")); //true
+    }
 }
