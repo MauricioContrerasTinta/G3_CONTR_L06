5ce5d3fbd9c27fad0f62348a1b735e8e96c75498
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:12:25 -0500

    prueba de cola con enteros

--- Test.java
+++ Test.java
@@ -3,5 +3,12 @@ import actividad2.Queue;
 import actividad1.ExceptionIsEmpty;
 
 public class Test {
-    
+    public static void main(String[] args) {
+        Queue<Integer> colaEnteros = new QueueArray<>(5);
+        colaEnteros.enqueue(10);
+        colaEnteros.enqueue(20);
+        colaEnteros.enqueue(30);
+
+        System.out.println("Cola actual: " + colaEnteros);
+    }
 }
