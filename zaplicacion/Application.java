bc6db1e31cf26c57e8dbb45d2533ea933f695f54
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:47:47 -0500

    arreglo errores

--- zaplicacion/Application.java
+++ zaplicacion/Application.java
@@ -3,30 +3,31 @@ import ejercicio1.StackLink;
 import actividad1.ExceptionIsEmpty;
 
 public class Application {
-    StackLink<Character> stack = new StackLink<>();
-    for (int i = 0; i < s.length(); i++) {
-        char simbolo = s.charAt(i);
-        if (simbolo == '(' || simbolo == '[' || simbolo == '{') {
-            stack.push(simbolo);
-        }
-        else if (simbolo == ')' || simbolo == ']' || simbolo == '}') {
-            if (stack.isEmpty()) return false;
+    public static boolean symbolBalancing(String s) {
+        StackLink<Character> stack = new StackLink<>();
+        for (int i = 0; i < s.length(); i++) {
+            char simbolo = s.charAt(i);
+            if (simbolo == '(' || simbolo == '[' || simbolo == '{') {
+                stack.push(simbolo);
+            }
+            else if (simbolo == ')' || simbolo == ']' || simbolo == '}') {
+                if (stack.isEmpty()) return false;
 
-            try {
-                char tope = stack.pop();
-                    if (!coincide(tope, simbolo)) return false;
-                } catch (ExceptionIsEmpty e) {
-                    return false;
+                try {
+                    char tope = stack.pop();
+                        if (!coincide(tope, simbolo)) return false;
+                    } catch (ExceptionIsEmpty e) {
+                        return false;
+                    }
                 }
             }
-    }
-        return stack.isEmpty();
-    }
+            return stack.isEmpty();
+        }
 
-    private static boolean coincide(char apertura, char cierre) {
-        return (apertura == '(' && cierre == ')') ||
-               (apertura == '[' && cierre == ']') ||
-               (apertura == '{' && cierre == '}');
-    }
+        private static boolean coincide(char apertura, char cierre) {
+            return  (apertura == '(' && cierre == ')') ||
+                    (apertura == '[' && cierre == ']') ||
+                    (apertura == '{' && cierre == '}');
+        }
 }
 
