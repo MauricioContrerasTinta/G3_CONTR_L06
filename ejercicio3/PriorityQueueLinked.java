239a36017a3b2e9872b2660f9e2d06ae187bd51a
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:20:34 -0500

    declarando atributos

--- ejercicio3/PriorityQueueLinked.java
+++ ejercicio3/PriorityQueueLinked.java
@@ -3,6 +3,7 @@ import actividad2.Queue;
 import actividad2.QueueLink;
 import actividad1.ExceptionIsEmpty;
 
-public class PriorityQueueLinked {
-    
+public class PriorityQueueLinked<E> implements PriorityQueue<E> {
+    private Queue<E>[] colas; //arreglo de colas
+    private int niveles; //cantidad de prioridades
 }
