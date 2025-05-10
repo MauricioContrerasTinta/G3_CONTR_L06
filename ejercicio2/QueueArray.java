aad86ce9b296569c92ccea5e51e3d470c8a3f4ab
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:08:20 -0500

    constructor que recibe el tamaño máximo del arreglo

--- ejercicio2/QueueArray.java
+++ ejercicio2/QueueArray.java
@@ -4,5 +4,17 @@ import actividad2.Queue; // Se usa la misma interfaz de la actividad 2
 import actividad1.ExceptionIsEmpty;
 
 public class QueueArray<E> implements Queue<E> {
-    
+    private E[] arreglo;
+    private int primero;
+    private int ultimo;
+    private int tamaño;
+    private int capacidad;
+
+    public QueueArray(int capacidad) {
+        this.capacidad = capacidad;
+        this.arreglo = (E[]) new Object[capacidad];
+        this.primero = 0;
+        this.ultimo = 0;
+        this.tamaño = 0;
+    }
 }
