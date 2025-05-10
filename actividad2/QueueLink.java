80d0cde93905e22ecbdec491c0a8b37ee8c8fd81
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 12:13:43 -0500

    nodos de inicio de cola y final

--- actividad2/QueueLink.java
+++ actividad2/QueueLink.java
@@ -1,6 +1,9 @@
 package actividad2;
 import actividad1.ExceptionIsEmpty;
 
-public class QueueLink {
+public class QueueLink<E> implements Queue<E> {
+
+    private Node<E> primero; 
+    private Node<E> ultimo;  
     
 }
