644c2e0a99eba3d5153abb2e00402becbd02bd17
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 12:56:03 -0500

    Creamos una cola de prioridad para enteros, donde la prioridad es también un entero

--- Test.java
+++ Test.java
@@ -3,5 +3,7 @@ import actividad3.PriorityQueueLinkSort;
 import actividad1.ExceptionIsEmpty;
 
 public class Test {
-    
+    public static void main(String[] args) {
+        PriorityQueue<Integer, Integer> colaPrioridad = new PriorityQueueLinkSort<>();
+    }
 }
