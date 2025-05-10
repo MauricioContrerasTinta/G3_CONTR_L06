4a65142c318222bcb426f32e9bce1b5375401f5d
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 12:56:31 -0500

    Insertar elementos con diferentes prioridades

--- Test.java
+++ Test.java
@@ -5,5 +5,9 @@ import actividad1.ExceptionIsEmpty;
 public class Test {
     public static void main(String[] args) {
         PriorityQueue<Integer, Integer> colaPrioridad = new PriorityQueueLinkSort<>();
+
+        colaPrioridad.enqueue(10, 2);
+        colaPrioridad.enqueue(20, 1); 
+        colaPrioridad.enqueue(30, 3);
     }
 }
