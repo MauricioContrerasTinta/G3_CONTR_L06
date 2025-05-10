cedcc86b1e7a329e5bfd4b0c2e8a123bd1bd6e68
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:07:32 -0500

    Implementación de una cola utilizando un arreglo

---
+++ ejercicio2/QueueArray.java
@@ -0,0 +1,8 @@
+package ejercicio2;
+
+import actividad2.Queue; // Se usa la misma interfaz de la actividad 2
+import actividad1.ExceptionIsEmpty;
+
+public class QueueArray<E> implements Queue<E> {
+    
+}
