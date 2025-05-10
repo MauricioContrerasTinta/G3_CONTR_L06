98b2914217a82ef8d8c8814974e0fa640d9b0c67
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 15:25:01 -0500

    Pruebas para la cola de prioridad con múltiples colas enlazadas.

---
+++ Test.java
@@ -0,0 +1,9 @@
+import ejercicio3.PriorityQueue;
+import ejercicio3.PriorityQueueLinked;
+import actividad1.ExceptionIsEmpty;
+
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+}
