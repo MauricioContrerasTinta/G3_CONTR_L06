b13c3540651cde284615929d92cac17da4bffb78
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 12:26:01 -0500

    Creamos una cola para enteros

--- Test.java
+++ Test.java
@@ -3,5 +3,7 @@ import actividad2.QueueLink;
 import actividad1.ExceptionIsEmpty;
 
 public class Test {
-    
+    public static void main(String[] args) {
+        Queue<Integer> colaEnteros = new QueueLink<>();
+    }
 }
