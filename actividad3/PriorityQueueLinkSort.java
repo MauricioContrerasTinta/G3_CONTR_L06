0468a558b7483cb75afa1a89ca3e2bcae0963abb
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-05-10 12:36:23 -0500

    Clase interna para encapsular el dato y la prioridad de un elemento

--- actividad3/PriorityQueueLinkSort.java
+++ actividad3/PriorityQueueLinkSort.java
@@ -1,6 +1,14 @@
 package actividad3;
 import actividad1.ExceptionIsEmpty;
 
-public class PriorityQueueLinkSort {
-    
+public class PriorityQueueLinkSort<E, N extends Comparable<N>> implements PriorityQueue<E, N> {
+    class EntryNode {
+        E data;  // Dato del elemento
+        N priority;  // Prioridad del elemento
+
+        EntryNode(E data, N priority) {
+            this.data = data;
+            this.priority = priority;
+        }
+    }
 }
